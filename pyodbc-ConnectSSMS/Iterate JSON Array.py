33ab8db2f1add155a0df41a891d93661f3f5924b
BhandiaRE <107486906+bhandiaRE@users.noreply.github.com>
2022-11-03 08:04:05 +0200

    Update Iterate JSON Array.py

--- pyodbc-ConnectSSMS/Iterate JSON Array.py
+++ pyodbc-ConnectSSMS/Iterate JSON Array.py
@@ -1,1 +1,14 @@
+import requests
+import json
 
+url = 'https://locator.sasol.com/api/station.json'
+
+r = requests.get(url)
+
+sampleDict = json.loads(r.text)
+print("Checking if nested JSON key exists or not")
+
+if 'stations' in sampleDict:
+  for sub in sampleDict['stations']:
+    if 'station_address' in sub:
+      print(sub['station_address'])
